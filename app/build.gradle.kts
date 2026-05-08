975d4cf9dbee47feaf73d098123293b46bf52f02
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-08 20:03:29 -0300

    feat: add home page view with scrolling logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/com/example/unieat/LoginActivity.java
+++ app/src/main/java/com/example/unieat/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.unieat;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +17,39 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnSouAluno = findViewById(R.id.btnSouAluno);
+        Button btnSouCozinha = findViewById(R.id.btnSouCozinha);
+
+        // Define as cores
+        int colorRed = Color.parseColor("#7B1C1C");
+        int colorWhite = Color.WHITE;
+        int colorInactive = Color.parseColor("#F0E8E8"); // Cor de fundo do toggle (desativado)
+
+        btnSouAluno.setOnClickListener(v -> {
+            // Selecionar Aluno (Fica Vermelho)
+            btnSouAluno.setBackgroundTintList(ColorStateList.valueOf(colorRed));
+            btnSouAluno.setTextColor(colorWhite);
+            
+            // Desmarcar Cozinha (Fica Transparente/Cinza)
+            btnSouCozinha.setBackgroundTintList(ColorStateList.valueOf(colorInactive));
+            btnSouCozinha.setTextColor(colorRed);
+        });
+
+        btnSouCozinha.setOnClickListener(v -> {
+            // Selecionar Cozinha (Fica Vermelho)
+            btnSouCozinha.setBackgroundTintList(ColorStateList.valueOf(colorRed));
+            btnSouCozinha.setTextColor(colorWhite);
+            
+            // Desmarcar Aluno (Fica Transparente/Cinza)
+            btnSouAluno.setBackgroundTintList(ColorStateList.valueOf(colorInactive));
+            btnSouAluno.setTextColor(colorRed);
         });
     }
 }
