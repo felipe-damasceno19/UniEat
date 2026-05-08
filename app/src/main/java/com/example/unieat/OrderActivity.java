153b97e10750fc473522934b971a31a4e3bbaa72
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-08 20:58:10 -0300

    fix: fixing home and order view navigation error

--- app/src/main/java/com/example/unieat/OrderActivity.java
+++ app/src/main/java/com/example/unieat/OrderActivity.java
@@ -15,7 +15,6 @@ public class OrderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_order);
 
         ImageView btnClose = findViewById(R.id.btnClose);
@@ -23,11 +22,5 @@ public class OrderActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         NavigationHelper.setupBottomNavigation(this, bottomNavigationView, R.id.nav_orders);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
-            return insets;
-        });
     }
 }
--- app/src/main/java/com/example/unieat/StudentHomeActivity.java
+++ app/src/main/java/com/example/unieat/StudentHomeActivity.java
@@ -2,11 +2,10 @@ package com.example.unieat;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,13 +14,7 @@ public class StudentHomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_student_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         NavigationHelper.setupBottomNavigation(this, bottomNavigationView, R.id.nav_home);
