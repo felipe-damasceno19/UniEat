b57a70dd29e35cfcc7e6cf348731796e5c6d4638
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-11 09:01:49 -0300

    feat: add Homepresenter with filter methods

---
+++ app/src/main/java/com/example/unieat/presenter/HomePresenter.java
@@ -0,0 +1,43 @@
+package com.example.unieat.presenter;
+
+import android.content.Context;
+
+import com.example.unieat.dao.DishDAO;
+import com.example.unieat.data.SessionManager;
+import com.example.unieat.model.Dish;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomePresenter {
+
+    private DishDAO dishDAO;
+    private SessionManager sessionManager;
+
+    public HomePresenter(Context context) {
+        dishDAO = new DishDAO(context);
+        sessionManager = new SessionManager(context);
+    }
+
+    public String getWelcomeMessage() {
+        return "Olá, " +sessionManager.getName() +" !";
+    }
+
+    public double getBalance() {
+        return sessionManager.getBalance();
+    }
+
+    public List<Dish> getFeaturedDishes() {
+        List<Dish> list = dishDAO.findAll();
+        List<Dish> availableDishes = new ArrayList<>();
+
+        for (Dish dish : list) {
+            if(dish.isAvailable()){
+                availableDishes.add(dish);
+            }
+        }
+
+        return availableDishes;
+    }
+
+}
