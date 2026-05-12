84566f71b99ee1718fd692aea9f855006a5b45a6
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-11 21:27:30 -0300

    feat: add UniEat logo

--- app/src/main/java/com/example/unieat/view/MenuActivity.java
+++ app/src/main/java/com/example/unieat/view/MenuActivity.java
@@ -2,11 +2,7 @@ package com.example.unieat.view;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.unieat.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
