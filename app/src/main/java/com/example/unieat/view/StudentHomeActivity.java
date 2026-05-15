f2aa636339e28f56fa5f3457e15efde142c17980
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-14 21:27:09 -0300

    refactor: changing cart icon

--- app/src/main/java/com/example/unieat/view/StudentHomeActivity.java
+++ app/src/main/java/com/example/unieat/view/StudentHomeActivity.java
@@ -1,14 +1,13 @@
 package com.example.unieat.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.example.unieat.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StudentHomeActivity extends AppCompatActivity {
 
@@ -19,5 +18,11 @@ public class StudentHomeActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         NavigationHelper.setupBottomNavigation(this, bottomNavigationView, R.id.nav_home);
+
+        FloatingActionButton fabCart = findViewById(R.id.fabCart);
+        fabCart.setOnClickListener(v -> {
+            Intent intent = new Intent(this, OrderActivity.class);
+            startActivity(intent);
+        });
     }
 }
